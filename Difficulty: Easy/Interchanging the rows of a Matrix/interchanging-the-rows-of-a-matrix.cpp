03883a88f7d21042019704eb621551043fbd06d4
Utkarsh Yadav <utkarshyadav221105@gmail.com>
2026-07-27 12:06:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Interchanging the rows of a Matrix/interchanging-the-rows-of-a-matrix.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    // Function to interchange the rows of a matrix.
+    void interchangeRows(vector<vector<int> > &matrix) {
+        // code here
+         int n = matrix.size();
+         for(int i =0 ; i < n/2 ; i++){
+             swap(matrix[i],matrix[n-1-i]);
+         }
+    }
+};
